5add0a2e5fda274798fef4763017c1af2a57904f
jmesplana <johnm.esplana@gmail.com>
2024-07-29 18:53:50 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mce-hospital-simulation/',
+  base: '/hospital-simulation/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-  },
-  esbuild: {
-    loader: "jsx",
-    include: /src\/.*\.jsx?$/,
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    },
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
   },
 })
