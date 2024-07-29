c44f6bb37a338b85aed771b0a7366dd8528b2e9b
jmesplana <johnm.esplana@gmail.com>
2024-07-29 18:12:47 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/mce-hospital-simulation/', // Update this if your repo name is different
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`
+      },
   esbuild: {
     loader: "jsx",
     include: /src\/.*\.jsx?$/,
